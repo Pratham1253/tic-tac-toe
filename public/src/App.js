8d4284c1cf25145a6c5760e4285e086fc57cdf30
Pratham Shah <156590787+Pratham1253@users.noreply.github.com>
2024-09-14 15:17:31 +0530

    added rest of files

---
+++ public/src/App.js
@@ -0,0 +1,10 @@
+import "./style.css";
+import Board from "./Board";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Ready to code TIC-TAC-TOE ?</h1>
+      <Board />
+    </div>
+  );
+}
